05f6cd26960e4ea5cbcec37bbfb89dcc772899d2
putrajojonomic <putra@jojonomic.com>
2020-07-01 23:41:08 +0700

    on progress installment

--- controllers/RequestController.go
+++ controllers/RequestController.go
@@ -2,10 +2,6 @@ package controllers
 
 import(
 	"strconv"
-	// "encoding/json"
-	// "io/ioutil"
-	// "./models"
-	// "fmt"
 )
 
 
@@ -13,9 +9,6 @@ func CreateDayMax(tempSave map[string]int, numbers int) string  {
 
 	tempSave["create_day_max"] = numbers
 	num := strconv.Itoa(numbers)
-
-	// file, _ := json.MarshalIndent(tempSave, "", " ")
-	// _ = ioutil.WriteFile("test.json", file, 0644)
 
 	callback := "Created max request with " +num+ " requests" 
 	
@@ -63,12 +56,30 @@ func FindByAmountRejected(tempSaveArr map[string][]string, loan string) string {
 	if amount % 1000000 == 0{
 		return "Sorry, doesn't found it"
 	}
-	
+
 	for key, arg := range tempSaveArr {
 		if arg[2] == loan{
 			s += key + " "
 
 		} 
 	}
+	return s
+}
+func Installment(tempSaveArr map[string][]string, loan string, month string) string {
+	// newArr := map[string][]string
+	dataLoan := tempSaveArr[loan]
+	if dataLoan == nil{
+		return "Data Loan Not Found"
+	}
+	newMonth, _ := strconv.Atoi(month)
+	// amount, _ := strconv.Atoi(dataLoan[2])
+	// amountByMonth := amount / newMonth 
+
+	s := " "
+	// newArr["header"] = {"Month", "Due Date", "Administration Fee", "Capital", "Fee"}
+	for i := 1; i <= newMonth; i++ {
+		s += ""
+
+	}	
 	return s
 }
