ce0c8be5f2fb64328a03c18d96d2a37e2b504efa
putrajojonomic <putra@jojonomic.com>
2020-07-01 23:21:09 +0700

    fix command find_by_amount_rejected, find_by_amount_accepted

--- controllers/RequestController.go
+++ controllers/RequestController.go
@@ -2,33 +2,73 @@ package controllers
 
 import(
 	"strconv"
-	"encoding/json"
-	"io/ioutil"
-	"./models"
-	"fmt"
+	// "encoding/json"
+	// "io/ioutil"
+	// "./models"
+	// "fmt"
 )
 
 
 func CreateDayMax(tempSave map[string]int, numbers int) string  {
+
 	tempSave["create_day_max"] = numbers
 	num := strconv.Itoa(numbers)
 
-	file, _ := json.MarshalIndent(tempSave, "", " ")
-	_ = ioutil.WriteFile("test.json", file, 0644)
+	// file, _ := json.MarshalIndent(tempSave, "", " ")
+	// _ = ioutil.WriteFile("test.json", file, 0644)
 
 	callback := "Created max request with " +num+ " requests" 
 	
 	return callback
 }
 
-func AddDataBorrower(tempSave map[string]int, data string) string {
-		var p = models.RequestModel{1, "Singh", 26}
-		file, _ := json.MarshalIndent(p, "", " ")
-		_ = ioutil.WriteFile("test.json", file, 0644)
+func AddDataBorrower(tempSaveArr map[string][]string, data string) string {
+	callback := "Success: "+ data
+	return callback
+}
+
+func CheckStatus(tempSaveArr map[string][]string, loan string) string {
+
+	dataLoan := tempSaveArr[loan]
+	if dataLoan == nil{
+		return "Data Loan Not Found"
+	}
+
+	amount, _ := strconv.Atoi(dataLoan[2])
+	if amount % 1000000 != 0{
+		return "Loan ID " +loan+ " is Rejected"
+
+	}
+	return "Loan ID " +loan+ " is Accepted"
+}
 
 
 
-	fmt.Println("sdf")
+func FindByAmountAccepted(tempSaveArr map[string][]string, loan string) string {
+	s := " "
 
-	return ""
+	for key, arg := range tempSaveArr {
+		if arg[2] == loan{
+			s += key + " "
+
+		} 
+	}
+	return s
+}
+
+func FindByAmountRejected(tempSaveArr map[string][]string, loan string) string {
+	s := " "
+	amount, _ := strconv.Atoi(loan)
+
+	if amount % 1000000 == 0{
+		return "Sorry, doesn't found it"
+	}
+	
+	for key, arg := range tempSaveArr {
+		if arg[2] == loan{
+			s += key + " "
+
+		} 
+	}
+	return s
 }
